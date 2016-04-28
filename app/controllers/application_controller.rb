8812a16ce08f1a85702f693a1fc0a19f94af99c4
James Doyley <jdoyley@gmail.com>
2016-04-28 10:18:15 +0100

    fix site map and accessibility

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,27 +5,27 @@ class ApplicationController < ActionController::Base
   def accessibility
     @branch    = Willow::Branch.find_by_name("Accessibility")
     @branches  = []
-    @branches += Willow::Tree.find_by_name('Top').branches.roots.order(:access_key)
-    @branches += Willow::Tree.find_by_name('Main').branches.roots.order(:access_key)
+    @branches += Willow::Branch.where(tree: 'Top').roots.order(:access_key)
+    @branches += Willow::Branch.where(tree: 'Main').roots.order(:access_key)
   end
   
   def site_map
 		@branch   = Willow::Branch.find_by_name("Site Map")
-    @branches = Willow::Tree.find_by_name('Main').branches.order(:positions_depth_cache)
+    @branches = Willow::Branch.where(tree: 'Main').branches.order(:positions_depth_cache)
   end
   
   def contact_us
     @page = Page.find_by_title("Contact Us")
-		@current_leaf = Willow::StaticPage.find_by_name("Contact Us")
-		if request.post?
-			if params[:name].blank? || params[:enquiry].blank? || (params[:telephone].blank? && params[:email].blank?)
-				flash[:alert] = "Fields marked with an asterisk (*) are required. Either a telephone number or email address must be provided."
-			else
-				Mailer.contact_us(params[:name], params[:telephone], params[:email], params[:enquiry]).deliver
-				flash[:notice] = "Your enquiry has been sent."
-				redirect_to thank_you_path
-			end
-		end
+    @current_leaf = Willow::StaticPage.find_by_name("Contact Us")
+    if request.post?
+      if params[:name].blank? || params[:enquiry].blank? || (params[:telephone].blank? && params[:email].blank?)
+        flash[:alert] = "Fields marked with an asterisk (*) are required. Either a telephone number or email address must be provided."
+      else
+        Mailer.contact_us(params[:name], params[:telephone], params[:email], params[:enquiry]).deliver
+        flash[:notice] = "Your enquiry has been sent."
+        redirect_to thank_you_path
+      end
+    end
   end
   
   def search
